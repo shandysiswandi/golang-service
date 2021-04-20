6297de99687c7441dbb5497d17a3b983cad1b488
Shandy Siswandi <shandysiswandi@gmail.com>
2021-04-20 22:07:16 +0700

    improve httpError

--- internal/infrastructure/app/injector.go
+++ internal/infrastructure/app/injector.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/shandysiswandi/echo-service/internal/config"
+	"github.com/shandysiswandi/echo-service/internal/util/response"
 )
 
 func Injection(cfg *config.Config) *echo.Echo {
@@ -15,8 +18,38 @@ func Injection(cfg *config.Config) *echo.Echo {
 	e.Server.WriteTimeout = 30 * time.Second
 	e.HTTPErrorHandler = httpError
 
-	e = middlewares(e)
+	e = middlewares(e, cfg)
 	e = router(e)
 
 	return e
+}
+
+func httpError(e error, c echo.Context) {
+	code := http.StatusInternalServerError
+	reason := make([]string, 0)
+
+	if he, ok := e.(*echo.HTTPError); ok {
+		if he.Code == http.StatusUnauthorized {
+			msgErr := e.Error()
+			reason = append(reason, getMessageError(msgErr))
+		}
+		code = he.Code
+	}
+	c.JSON(code, response.ErrorBody{
+		Error:   true,
+		Message: http.StatusText(code),
+		Reason:  reason,
+	})
+}
+
+func getMessageError(msg string) string {
+	tmp := strings.Split(msg, ",")
+	tmpMsg := ""
+	if len(tmp) > 2 {
+		tmpMsg = tmp[1]
+		spl := strings.Split(tmpMsg, "=")
+		tmpMsg = spl[1]
+	}
+
+	return tmpMsg
 }
