317be7ceb49ba4e7e43850ea12d68d7a67966421
Shandy Siswandi <shandysiswandi@gmail.com>
2021-04-21 19:39:11 +0700

    improve injector

--- internal/infrastructure/app/injector_test.go
+++ internal/infrastructure/app/injector_test.go
@@ -46,5 +46,35 @@ func Test_HTTPError_Unauthorized(t *testing.T) {
 
 	e.ServeHTTP(rec, req)
 	assert.Equal(t, http.StatusUnauthorized, rec.Code)
-	assert.Equal(t, "{\"error\":true,\"message\":\"Unauthorized\",\"reason\":[\"invalid or expired jwt\"]}\n", rec.Body.String())
+	assert.Equal(t, "{\"error\":true,\"message\":\"Unauthorized\",\"reason\":[\"token invalid or expired\"]}\n", rec.Body.String())
 }
+
+func Test_HTTPError_BadRequest(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/jwt", nil)
+	rec := httptest.NewRecorder()
+
+	token := ""
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	cfg := &config.Config{JWTSecret: ""}
+	e := app.Injection(cfg)
+
+	e.ServeHTTP(rec, req)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Equal(t, "{\"error\":true,\"message\":\"Bad Request\",\"reason\":[\"token not provide\"]}\n", rec.Body.String())
+}
+
+// func Test_HTTPError_InternalServerError(t *testing.T) {
+// 	req := httptest.NewRequest(http.MethodGet, "/panic", nil)
+// 	rec := httptest.NewRecorder()
+
+// 	token := "a"
+// 	req.Header.Set("Authorization", "Bearer "+token)
+
+// 	cfg := &config.Config{JWTSecret: ""}
+// 	e := app.Injection(cfg)
+
+// 	e.ServeHTTP(rec, req)
+// 	assert.Equal(t, http.StatusBadRequest, rec.Code)
+// 	assert.Equal(t, "{\"error\":true,\"message\":\"Bad Request\",\"reason\":[\"missing or malformed jwt\"]}\n", rec.Body.String())
+// }
