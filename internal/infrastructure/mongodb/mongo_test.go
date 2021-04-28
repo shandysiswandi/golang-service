6a46dc3d06b055e8d8f4acc226d15a33d3ee6e0b
Shandy Siswandi <shandysiswandi@gmail.com>
2021-04-28 20:39:42 +0700

    update mongo_test

--- internal/infrastructure/mongodb/mongo_test.go
+++ internal/infrastructure/mongodb/mongo_test.go
@@ -1,11 +1,80 @@
 package mongodb_test
 
-import "testing"
+import (
+	"testing"
 
-func Test_New(t *testing.T) {}
+	"github.com/shandysiswandi/echo-service/internal/config"
+	"github.com/shandysiswandi/echo-service/internal/infrastructure/mongodb"
+	"github.com/stretchr/testify/assert"
+)
 
-func Test_Connect(t *testing.T) {}
+func Test_New(t *testing.T) {
+	cfg := &config.Config{}
+	act := mongodb.New(cfg)
+	exp := &mongodb.MongoDB{}
 
-func Test_GetDB(t *testing.T) {}
+	assert.NotEqual(t, exp, act)
+}
 
-func Test_Close(t *testing.T) {}
+func Test_Connect(t *testing.T) {
+	mongo := []config.Mongo{{
+		URI:            "mongodb://localhost:27017/",
+		ConnectionName: "default",
+		DatabaseName:   "echo-service-test",
+	}}
+	cfg := &config.Config{Mongos: mongo}
+	mdb := mongodb.New(cfg)
+	act := mdb.Connect()
+
+	assert.NoError(t, act)
+
+	// error connect to mongo beaculse invalid `uri`
+	mongo = []config.Mongo{{
+		URI:            "mongdb://localhost:27010/",
+		ConnectionName: "default",
+		DatabaseName:   "echo-service-test",
+	}}
+	cfg = &config.Config{Mongos: mongo}
+	mdb = mongodb.New(cfg)
+	act = mdb.Connect()
+
+	assert.Error(t, act)
+
+	// error connect to mongo can not ping mongo
+	mongo = []config.Mongo{{
+		URI:            "mongodb://localhost:27010/",
+		ConnectionName: "default",
+		DatabaseName:   "echo-service-test",
+	}}
+	cfg = &config.Config{Mongos: mongo}
+	mdb = mongodb.New(cfg)
+	act = mdb.Connect()
+
+	assert.Error(t, act)
+}
+
+func Test_GetDB(t *testing.T) {
+	mongo := []config.Mongo{{
+		URI:            "mongodb://localhost:27017/",
+		ConnectionName: "default",
+		DatabaseName:   "echo-service-test",
+	}}
+	cfg := &config.Config{Mongos: mongo}
+	mdb := mongodb.New(cfg)
+	act := mdb.GetDB()
+
+	assert.NotNil(t, act)
+	mdb.Close()
+}
+
+func Test_Close(t *testing.T) {
+	mongo := []config.Mongo{{
+		URI:            "mongodb://localhost:27017/",
+		ConnectionName: "default",
+		DatabaseName:   "echo-service-test",
+	}}
+	cfg := &config.Config{Mongos: mongo}
+	mdb := mongodb.New(cfg)
+
+	mdb.Close()
+}
