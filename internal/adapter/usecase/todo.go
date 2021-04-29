4e31571b0c6f16728ad1d699247d8d6792487a7a
Shandy Siswandi <shandysiswandi@gmail.com>
2021-04-30 02:58:20 +0700

    update usecaseTodo

--- internal/adapter/usecase/todo.go
+++ internal/adapter/usecase/todo.go
@@ -2,9 +2,11 @@ package usecase
 
 import (
 	"context"
+	"time"
 
 	"github.com/shandysiswandi/echo-service/internal/domain"
 	"github.com/shandysiswandi/echo-service/internal/port"
+	"github.com/shandysiswandi/echo-service/pkg/nanoid"
 )
 
 type todoUsecase struct {
@@ -15,26 +17,46 @@ func NewTodoUsecase(tdu port.TodoRepository) port.TodoUsecase {
 	return &todoUsecase{tdu}
 }
 
-func (tdu *todoUsecase) FetchTodos(context.Context) ([]*domain.Todo, error) {
-	return nil, nil
+func (tdu *todoUsecase) FetchTodos(ctx context.Context) ([]*domain.Todo, error) {
+	return tdu.todoRepo.Fetch(ctx)
 }
 
-func (tdu *todoUsecase) GetTodoByID(context.Context, string) (*domain.Todo, error) {
-	return nil, nil
+func (tdu *todoUsecase) GetTodoByID(ctx context.Context, id string) (*domain.Todo, error) {
+	return tdu.todoRepo.GetByID(ctx, id)
 }
 
-func (tdu *todoUsecase) CreateTodo(context.Context, domain.TodoCreatePayload) error {
-	return nil
+func (tdu *todoUsecase) CreateTodo(ctx context.Context, payload domain.TodoCreatePayload) error {
+	id, _ := nanoid.Generate(11)
+	data := domain.Todo{
+		ID:          id,
+		Title:       payload.Title,
+		Description: payload.Description,
+		Completed:   false,
+		Timestamp:   time.Now(),
+	}
+	return tdu.todoRepo.Create(ctx, data)
 }
 
-func (tdu *todoUsecase) UpdateTodoByID(context.Context, domain.TodoUpdatePayload) error {
-	return nil
+func (tdu *todoUsecase) UpdateTodoByID(ctx context.Context, payload domain.TodoUpdatePayload) error {
+	data := domain.Todo{
+		ID:          payload.ID,
+		Title:       payload.Title,
+		Description: payload.Description,
+		Completed:   payload.Completed,
+	}
+	return tdu.todoRepo.UpdateByID(ctx, data)
 }
 
-func (tdu *todoUsecase) ReplaceTodoByID(context.Context, domain.TodoReplacePayload) error {
-	return nil
+func (tdu *todoUsecase) ReplaceTodoByID(ctx context.Context, payload domain.TodoReplacePayload) error {
+	data := domain.Todo{
+		ID:          payload.ID,
+		Title:       payload.Title,
+		Description: payload.Description,
+		Completed:   payload.Completed,
+	}
+	return tdu.todoRepo.ReplaceByID(ctx, data)
 }
 
-func (tdu *todoUsecase) DeleteTodoByID(context.Context, string) error {
-	return nil
+func (tdu *todoUsecase) DeleteTodoByID(ctx context.Context, id string) error {
+	return tdu.todoRepo.DeleteByID(ctx, id)
 }
