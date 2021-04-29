38b35f67b2144ed7f9dc844c2ddd6ac4c09a4ac7
Shandy Siswandi <shandysiswandi@gmail.com>
2021-04-30 02:58:29 +0700

    update todo domain

--- internal/domain/todo.go
+++ internal/domain/todo.go
@@ -1,13 +1,20 @@
 package domain
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrTodoNotFound = errors.New("todo not found")
+)
 
 type Todo struct {
 	ID          string    `json:"id" bson:"id"`
-	Title       string    `json:"title" bson:"title"`
-	Description string    `json:"description" bson:"description"`
-	Completed   bool      `json:"completed" bson:"completed"`
-	Timestamp   time.Time `json:"timestamp" bson:"timestamp"`
+	Title       string    `json:"title" bson:"title,omitempty"`
+	Description string    `json:"description" bson:"description,omitempty"`
+	Completed   bool      `json:"completed" bson:"completed,omitempty"`
+	Timestamp   time.Time `json:"timestamp" bson:"timestamp,omitempty"`
 }
 
 type TodoCreatePayload struct {
@@ -16,12 +23,14 @@ type TodoCreatePayload struct {
 }
 
 type TodoUpdatePayload struct {
-	Title       string `json:"title" validate:"required"`
-	Description string `json:"description" validate:"required"`
-	Completed   bool   `json:"completed" validate:"required"`
+	ID          string `json:"id"`
+	Title       string `json:"title" validate:"omitempty"`
+	Description string `json:"description" validate:"omitempty"`
+	Completed   bool   `json:"completed" validate:"omitempty"`
 }
 
 type TodoReplacePayload struct {
+	ID          string `json:"id"`
 	Title       string `json:"title" validate:"required"`
 	Description string `json:"description" validate:"required"`
 	Completed   bool   `json:"completed" validate:"required"`
