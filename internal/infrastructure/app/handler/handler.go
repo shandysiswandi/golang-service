c4d7e0b7b709de18441a464d1e0e0ff62447639b
Shandy Siswandi <shandysiswandi@gmail.com>
2021-04-30 02:59:34 +0700

    add validation & todoUsecase

--- internal/infrastructure/app/handler/handler.go
+++ internal/infrastructure/app/handler/handler.go
@@ -1,15 +1,30 @@
 package handler
 
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/shandysiswandi/echo-service/internal/port"
+)
+
 /* Example get user jwt
 user := c.Get("user").(*jwte.Token)
 data := user.Claims.(*JWTClaim)
 */
 
 type (
-	handler       struct{}
-	HandlerConfig struct{}
+	handler struct {
+		validate *validator.Validate
+		tdu      port.TodoUsecase
+	}
+
+	HandlerConfig struct {
+		TodoUsecase port.TodoUsecase
+		Validator   *validator.Validate
+	}
 )
 
 func New(hc HandlerConfig) *handler {
-	return &handler{}
+	return &handler{
+		validate: hc.Validator,
+		tdu:      hc.TodoUsecase,
+	}
 }
