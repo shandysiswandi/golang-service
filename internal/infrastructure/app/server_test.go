70c942f815b3b2273ad051fe0d6dbcb8ad0db47a
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-05-02 02:19:57 +0700

    refactor to simple test

--- internal/infrastructure/app/server_test.go
+++ internal/infrastructure/app/server_test.go
@@ -4,21 +4,16 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/shandysiswandi/echo-service/internal/config"
-	"github.com/shandysiswandi/echo-service/internal/infrastructure/app"
 	"github.com/shandysiswandi/echo-service/internal/infrastructure/app/tester"
-	"github.com/shandysiswandi/echo-service/internal/infrastructure/mongodb"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHTTPErrorMethodNotAllowed(t *testing.T) {
 	// setup
-	cfg := config.New()
-	dbm := mongodb.New(cfg)
-	e := app.New(cfg, dbm)
+	testy := tester.New()
 
 	// testing
-	code, body := tester.RequestTest(e, http.MethodPost, "/", nil)
+	code, body := testy.RequestWithServe(http.MethodPost, "/", nil, nil)
 
 	// assertion
 	assert.Equal(t, http.StatusMethodNotAllowed, code)
@@ -27,29 +22,12 @@ func TestHTTPErrorMethodNotAllowed(t *testing.T) {
 
 func TestHTTPErrorNotFound(t *testing.T) {
 	// setup
-	cfg := config.New()
-	dbm := mongodb.New(cfg)
-	e := app.New(cfg, dbm)
+	testy := tester.New()
 
 	// testing
-	code, body := tester.RequestTest(e, http.MethodGet, "/xxx", nil)
+	code, body := testy.RequestWithServe(http.MethodGet, "/xxx", nil, nil)
 
 	// assertion
 	assert.Equal(t, http.StatusNotFound, code)
 	assert.Equal(t, "{\"error\":true,\"message\":\"Not Found\",\"reason\":[]}\n", body)
 }
-
-// func TestHTTPErrorInternalServerError(t *testing.T) {
-// 	req := httptest.NewRequest(http.MethodGet, "/panic", nil)
-// 	rec := httptest.NewRecorder()
-
-// 	token := "a"
-// 	req.Header.Set("Authorization", "Bearer "+token)
-
-// 	cfg := &config.Config{JWTSecret: ""}
-// 	e := app.Injection(cfg)
-
-// 	e.ServeHTTP(rec, req)
-// 	assert.Equal(t, http.StatusBadRequest, rec.Code)
-// 	assert.Equal(t, "{\"error\":true,\"message\":\"Bad Request\",\"reason\":[\"missing or malformed jwt\"]}\n", rec.Body.String())
-// }
