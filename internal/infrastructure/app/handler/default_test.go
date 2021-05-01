2fc44909103827e05e6d394633e1e1cd3a62018a
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-05-01 21:33:25 +0700

    update defaulthandler test

--- internal/infrastructure/app/handler/default_test.go
+++ internal/infrastructure/app/handler/default_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/shandysiswandi/echo-service/internal/config"
 	"github.com/shandysiswandi/echo-service/internal/infrastructure/app"
 	"github.com/shandysiswandi/echo-service/internal/infrastructure/app/handler"
+	"github.com/shandysiswandi/echo-service/internal/infrastructure/mongodb"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,58 +16,58 @@ func TestHome(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 
-	cfg := &config.Config{JWTSecret: ""}
-	e := app.New(cfg)
-
+	// setup
+	cfg := config.New()
+	dbm := mongodb.New(cfg)
+	e := app.New(cfg, dbm)
 	c := e.NewContext(req, rec)
 
+	// testing
 	hc := handler.HandlerConfig{}
 	h := handler.New(hc)
-
-	expBody := "welcome home"
 
 	// Assertions
 	assert.NoError(t, h.Home(c))
 	assert.Equal(t, http.StatusOK, rec.Code)
-	assert.Equal(t, expBody, rec.Body.String())
+	assert.Equal(t, "welcome home", rec.Body.String())
 }
 
 func TestHealth(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/health", nil)
 	rec := httptest.NewRecorder()
 
-	cfg := &config.Config{JWTSecret: ""}
-	e := app.New(cfg)
-
+	// setup
+	cfg := config.New()
+	dbm := mongodb.New(cfg)
+	e := app.New(cfg, dbm)
 	c := e.NewContext(req, rec)
 
+	// testing
 	hc := handler.HandlerConfig{}
 	h := handler.New(hc)
-
-	expBody := "health"
 
 	// Assertions
 	assert.NoError(t, h.Health(c))
 	assert.Equal(t, http.StatusOK, rec.Code)
-	assert.Equal(t, expBody, rec.Body.String())
+	assert.Equal(t, "health", rec.Body.String())
 }
 
 func TestGraceful(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/graceful", nil)
 	rec := httptest.NewRecorder()
 
-	cfg := &config.Config{JWTSecret: ""}
-	e := app.New(cfg)
-
+	// setup
+	cfg := config.New()
+	dbm := mongodb.New(cfg)
+	e := app.New(cfg, dbm)
 	c := e.NewContext(req, rec)
 
+	// testing
 	hc := handler.HandlerConfig{}
 	h := handler.New(hc)
-
-	expBody := "OK"
 
 	// Assertions
 	assert.NoError(t, h.Graceful(c))
 	assert.Equal(t, http.StatusOK, rec.Code)
-	assert.Equal(t, expBody, rec.Body.String())
+	assert.Equal(t, "OK", rec.Body.String())
 }
