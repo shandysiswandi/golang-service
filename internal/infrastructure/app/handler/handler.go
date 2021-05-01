c46d1465f6acb677b768d670236c0e51e4411665
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-05-02 05:05:28 +0700

    add field param config

--- internal/infrastructure/app/handler/handler.go
+++ internal/infrastructure/app/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/shandysiswandi/echo-service/internal/config"
 	"github.com/shandysiswandi/echo-service/internal/port"
 	"github.com/shandysiswandi/echo-service/pkg/validation"
 )
@@ -12,11 +13,13 @@ data := user.Claims.(*JWTClaim)
 
 type (
 	handler struct {
+		config   *config.Config
 		validate *validation.Validation
 		tdu      port.TodoUsecase
 	}
 
 	HandlerConfig struct {
+		Config      *config.Config
 		Validator   *validation.Validation
 		TodoUsecase port.TodoUsecase
 	}
@@ -24,6 +27,7 @@ type (
 
 func New(hc HandlerConfig) *handler {
 	return &handler{
+		config:   hc.Config,
 		validate: hc.Validator,
 		tdu:      hc.TodoUsecase,
 	}
