35c5ba5b9ab758acfe4eddc2817e5a2104edf8f7
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-05-03 22:34:05 +0700

    change to exported struct

--- internal/infrastructure/app/handler/default.go
+++ internal/infrastructure/app/handler/default.go
@@ -8,20 +8,20 @@ import (
 	"github.com/shandysiswandi/echo-service/internal/infrastructure/app/middle"
 )
 
-func (h *handler) Home(c echo.Context) error {
+func (h *Handler) Home(c echo.Context) error {
 	return c.String(http.StatusOK, "welcome home")
 }
 
-func (h *handler) Health(c echo.Context) error {
+func (h *Handler) Health(c echo.Context) error {
 	return c.String(http.StatusOK, "health")
 }
 
-func (h *handler) Graceful(c echo.Context) error {
+func (h *Handler) Graceful(c echo.Context) error {
 	time.Sleep(1 * time.Second)
 	return c.String(http.StatusOK, "OK")
 }
 
-func (h *handler) JWT(c echo.Context) error {
+func (h *Handler) JWT(c echo.Context) error {
 	accToken, _ := middle.CreateJWTToken("secret")
 	return c.String(http.StatusOK, accToken)
 }
